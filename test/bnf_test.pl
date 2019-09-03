5ab6f1df64dec12bd7a86fef00172a9b7c257ee2
Jose Acuña <jodaniel1409@gmail.com>
2019-09-03 00:21:17 -0600

    Created a unitary test for the bnf

---
+++ test/bnf_test.pl
@@ -0,0 +1,8 @@
+:-consult('../src/bnf/bnf.pl').
+
+% Ejectutar "run." en la consola para correr la prueba
+% Estructura no definitiva, sera cambiada
+
+run() :-
+	oracion([el,hombre,correr,la,hombre], []),
+	oracion([el,hombre,comer,la,manzana], []).
