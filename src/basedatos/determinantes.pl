c4451bcb9a3b3260671816cda1384ce80abdd521
Jose Acuña <jodaniel1409@gmail.com>
2019-09-02 21:08:47 -0600

    Added some determinants

--- src/basedatos/determinantes.pl
+++ src/basedatos/determinantes.pl
@@ -1,1 +1,19 @@
 % Determinantes
+
+% Estructura de la base de datos:
+% arg0 determinante
+% arg1 cantidad S:singular y P:plural
+% arg2 genero M:masculino y F:femenino
+
+determinante(el, S, M).
+determinante(la, S, F).
+determinante(lo, S, N).
+determinante(los, P, N).
+
+determinante(un, S, M).
+determinante(una, S, F).
+determinante(unos, P, M).
+determinante(unas, P, F).
+
+determinante(mi, S, N).
+determinante(mis, P, N).
