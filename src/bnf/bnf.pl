e2fa5982b94009c164ed47cde64caa528a03bd0e
Jose Acuña <jodaniel1409@gmail.com>
2019-09-03 22:13:33 -0600

    BNF now checks for sentence concordance

--- src/bnf/bnf.pl
+++ src/bnf/bnf.pl
@@ -1,22 +1,25 @@
 % BNF
+% Referencia LibroPROLOG Proyecto5
+% Author GitHub@JoDaniel1412
+% Ultimo cambio 03/09/2019 por @JoDaniel1412
 
 
 % Estructura de la oracion %
 oracion(S0, S) :-
-	sintagma_nominal(S0, S1),
-	sintagma_verbal(S1, S).
+	sintagma_nominal(NUM, _, PERS, S0, S1),
+	sintagma_verbal(NUM, _, PERS, S1, S).
 
 
 % Sintagmas Nominales %
-sintagma_nominal(S0, S) :-
-	determinante(S0, S1),
-	sustantivo(S1, S).
+sintagma_nominal(NUM, GEN, PERS, S0, S) :-
+	determinante(NUM, GEN, PERS, S0, S1),
+	sustantivo(NUM, GEN, S1, S).
 
 
 % Sintagmas Verbales %
-sintagma_verbal(S0, S) :-
-	verbo(S0, S1),
-	sintagma_nominal(S1, S).
+sintagma_verbal(NUM, _, PERS, S0, S) :-
+	verbo(NUM, PERS, S0, S1),
+	sintagma_nominal(_, _, _, S1, S).
 
-sintagma_verbal(S0, S) :- 
-	verbo(S0, S).
+sintagma_verbal(NUM, _, PERS, S0, S) :- 
+	verbo(NUM, PERS, S0, S).
