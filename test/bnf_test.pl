87688a1b926770a8a27beaab2087c9a61a2db3c7
Jose Acuña <jodaniel1409@gmail.com>
2019-09-03 22:19:55 -0600

    Improved the unit test structure

--- test/bnf_test.pl
+++ test/bnf_test.pl
@@ -1,8 +1,20 @@
 :-consult('../src/bnf/bnf.pl').
+:-consult('../src/basedatos/basedatos.pl').
 
 % Ejectutar "run." en la consola para correr la prueba
 % Estructura no definitiva, sera cambiada
 
+
+prueba_incoherencia() :-
+	write("prueba_incoherencia: \n"),
+	( not(oracion([el,hombre,correr,la,hombre], [])) -> write("OK \n") ; write("FAIL \n") ),
+	( not(oracion([el,hombre,come,el,personas], [])) -> write("OK \n") ; write("FAIL \n") ).
+
+prueba_coherencia() :-
+	write("prueba_coherencia: \n"),
+	( oracion([la,mujer,come,la,fruta], []) -> write("OK \n") ; write("FAIL \n") ),
+	( oracion([el,hombre,come,la,manzana], []) -> write("OK \n") ; write("FAIL \n") ).
+
 run() :-
-	oracion([el,hombre,correr,la,hombre], []),
-	oracion([el,hombre,comer,la,manzana], []).
+	prueba_incoherencia,
+	prueba_coherencia.
