34c8438b602ce2b654adc5c5abb4ff75af8d3244
Jose Acuña <jodaniel1409@gmail.com>
2019-09-04 00:26:33 -0600

    Added an user interface

--- src/basedatos/claves.pl
+++ src/basedatos/claves.pl
@@ -1,1 +1,6 @@
 % Palabras claves
+
+bot("CallCenterBot").
+usuario("Usuario").
+
+saludo("Buenas, en que le puedo ayudar?").
--- src/main.pl
+++ src/main.pl
@@ -1,3 +1,38 @@
 
 :-consult('bnf/bnf.pl').
 :-consult('basedatos/basedatos.pl').
+
+
+iniciar() :-
+	saludar(),
+	conversar().
+
+
+saludar() :-
+	imprimir_nombre(bot),
+	saludo(S),
+	write(S),
+	nl().
+
+
+conversar() :-
+	repeat(),
+
+	imprimir_nombre(usuario),
+	read(M),
+
+	imprimir_nombre(bot),
+	analizar_respuesta(M, R),
+	write(R), 
+	nl(), !.
+
+
+analizar_respuesta(M, R) :-
+	M == R.
+	
+
+imprimir_nombre(bot) :-
+	bot(N), write(N), write(": ").
+
+imprimir_nombre(usuario) :-
+	usuario(N), write(N), write(": ").
